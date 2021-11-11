64a90f9d5819d73c30872e7cd5322f2016b70a22
Shiho <mia.can2021@gmail.com>
2021-11-10 18:16:49 -0800

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>VanCafe</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -16,7 +16,7 @@ export default function Home() {
           </a>
         </h1>
 
-        <p className="mt-3 text-2xl">
+        <p className="mt-3 text-lg">
           Get started by editing{' '}
           <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
             pages/index.js
