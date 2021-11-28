975378a6e9a889f1071b34e811811958ee7fd249
Shiho <mia.can2021@gmail.com>
2021-11-27 23:41:14 -0800

    add googlemap api for slug page

--- pages/new/[slug].js
+++ pages/new/[slug].js
@@ -1,12 +1,22 @@
 import { useRouter } from 'next/router'
 import { getPost, getAllPostsSlug} from "../../lib/getPost";
 import Error from '../error';
+import GoogleMapReact from 'google-map-react';
+
 
 const post = ({postData}) =>{
     const router = useRouter()
     if(!router.isFallback && !postData?.slug){
         return <Error status={404} />
     }
+    const mapLocation = {
+        center:{
+        lat: postData.location.lat,
+        lng: postData.location.lng,
+        }
+    }
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
     return(
         <div>
             {router.isFallback?(
@@ -15,11 +25,26 @@ const post = ({postData}) =>{
                 (
                 <div className="bg-primary text-b-n w-screen h-auto">
                 <div className="prose-2xl text-center font-title pt-6" dangerouslySetInnerHTML={{__html: postData.title}}/>
-                <div className="prose text-center break-words lg:prose-x prose-indigo font-para w-9/12 mx-auto" dangerouslySetInnerHTML={{__html: postData.content}}/>
+                <div className="prose text-center break-words lg:prose-x prose-indigo font-para w-9/12 h-auto flex flex-col items-center justify-center mx-auto rounded-sm " dangerouslySetInnerHTML={{__html: postData.content}}/>
+                <div className="mx-auto" style={{ height: '300px', width: '300px' }}>
+                     <GoogleMapReact
+                     bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY}}
+                     defaultCenter={mapLocation.center}
+                     defaultZoom={14}
+                     > 
+                     <AnyReactComponent
+                     lat={postData.location.lat}
+                     lng={postData.location.lng}
+                     text="My Marker"
+                   />
+                     </GoogleMapReact>
                 </div>
-            )}
-        </div>
+                </div>
+                   
     )
+    }
+        </div>
+    )   
 }
 
 export default post;
