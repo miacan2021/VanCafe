77b8e88876897500597ab75571dc7f1fdfc23b17
Shiho <mia.can2021@gmail.com>
2021-11-28 16:58:42 -0800

    add pin at google map

--- pages/new/[slug].js
+++ pages/new/[slug].js
@@ -1,22 +1,45 @@
-import { useRouter } from 'next/router'
+import { useRouter} from 'next/router'
+import { useState } from 'react';
 import { getPost, getAllPostsSlug} from "../../lib/getPost";
 import Error from '../error';
 import GoogleMapReact from 'google-map-react';
+import Link from 'next/link';
 
 
 const post = ({postData}) =>{
+    const [open, setOpen] = useState(false)
+
     const router = useRouter()
     if(!router.isFallback && !postData?.slug){
         return <Error status={404} />
     }
+    const latData = postData.location.lat
+    const lngData = postData.location.lng
     const mapLocation = {
         center:{
-        lat: postData.location.lat,
-        lng: postData.location.lng,
+        lat: latData,
+        lng: lngData,
         }
     }
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Pin = () => {
+    return(
+     <svg onClick={handleOpen} className="w-8 h-8 cursor-pointer" fill="bg-b-n" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
+    )
+}
+const handleOpen = () => {
+    setOpen(!open)
+}
+
+const CafeDetail = () => {
+return (
+<div onClick={handleOpen} className="cursor-pointer bg-white w-32 h-auto p-1 text-b-n rounded-sm flex flex-col justify-center items-center">
+    <p>{postData.title}</p>
+    <p>{postData.time.open} - {postData.time.close}</p>
+    <Link href={`https://maps.google.com?q=${latData},${lngData}`}><a className="text-b-p">View on Google Maps</a></Link>
+</div>
+    )
+}
     return(
         <div>
             {router.isFallback?(
@@ -32,12 +55,13 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
                      defaultCenter={mapLocation.center}
                      defaultZoom={14}
                      > 
-                     <AnyReactComponent
-                     lat={postData.location.lat}
-                     lng={postData.location.lng}
-                     text="My Marker"
-                   />
-                     </GoogleMapReact>
+                   
+                   {open ?
+                   <CafeDetail lat={latData} lng={lngData} />
+                    :
+                    <Pin lat={latData} lng={lngData} />
+                     }
+                    </GoogleMapReact>
                 </div>
                 </div>
                    
