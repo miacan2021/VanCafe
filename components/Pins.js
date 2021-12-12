e42e7a6ad54f5a59e40f4c824690f02be94f97f3
Shiho <mia.can2021@gmail.com>
2021-12-12 00:09:44 -0800

    done with the map-search page

--- components/Pins.js
+++ components/Pins.js
@@ -2,10 +2,9 @@ import React from 'react'
 import {Marker} from 'react-map-gl';
 
 const Pins = ({data, setPopupInfo}) => {
-     
     return data.map(info => (
         <Marker key={info.title} longitude={info.location.lng} latitude={info.location.lat}>
-        <svg onClick={()=> setPopupInfo(info)} xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11.5" cy="8.5" r="5.5"/><path d="M11.5 14v7"/></svg>          
+        <svg onClick={()=> setPopupInfo(info)} xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="none" stroke="#253D55" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11.5" cy="8.5" r="5.5"/><path d="M11.5 14v7"/></svg>          
         </Marker>
   ))
 }
--- pages/map-search.js
+++ pages/map-search.js
@@ -1,27 +1,21 @@
 import { useState } from 'react';
-import ReactMapGL, {FullscreenControl, Popup} from 'react-map-gl';
+import ReactMapGL, {Popup} from 'react-map-gl';
 import Pins from '../components/Pins';
 import getAllMapData from '../lib/getAllMapData';
 import MapCard from '../components/MapCard';
-import Link from 'next/link';
+import MobileMapCard from '../components/MobileMapCard';
 
 const token = process.env.NEXT_PUBLIC_MAP_BOX_TOKEN
 
-const fullscreenControlStyle= {
-    right: 10,
-    top: 10
-  };
-  
-
 const mapSearch = ({mapData}) => {
-    //target map
     const [viewport, setViewport] = useState({
         latitude: 49.246292,
         longitude: -123.116226,
-        zoom: 11.8,
+        zoom: 12
       });
 
       const [popupInfo, setPopupInfo] = useState(null);
+
       return (
           <>
         <div className="mapbox-react relative">
@@ -29,11 +23,10 @@ const mapSearch = ({mapData}) => {
             {...viewport}
             width="100vw"
             height="100vh"
-            mapStyle="mapbox://styles/mapbox/streets-v11"
+            style='mapbox://styles/miacan2021/ckx2yfuly4moi14lg6ny9mqja'
             onViewportChange={nextViewport => setViewport(nextViewport)}
             mapboxApiAccessToken={token}
           >
-        <FullscreenControl style={fullscreenControlStyle} />
         <Pins data={mapData} setPopupInfo={setPopupInfo} />
         {popupInfo && !window.matchMedia('(max-width: 425px)').matches ? 
           <Popup
@@ -43,22 +36,12 @@ const mapSearch = ({mapData}) => {
             latitude={popupInfo.location.lat}
             closeOnClick={false}
             onClose={setPopupInfo}
+            onClick={() => setPopupInfo(null)} 
           >
           <MapCard data={popupInfo} />
           </Popup>
         : popupInfo && window.matchMedia('(max-width: 425px)').matches ? 
-       <div className='w-screen h-1/4 bg-primary absolute bottom-0 left-0 flex items-center justify-center overflow-hidden'>
-       <img src={popupInfo.featuredImage.node.sourceUrl} alt="cafe image" className='w-1/2 object-cover' />
-       <div class="p-2 w-1/2 text-center">
-       <p className="text-sm mb-2">{popupInfo.title}</p>
-       <p className="text-xs font-pra mb-1">{popupInfo.adress.adress}</p>
-       <p className="font-pra whitespace-normal text-xs">{popupInfo.time.open} - {popupInfo.time.close}</p>
-       <Link href={`https://maps.google.com?q=${popupInfo.location.lat},${popupInfo.location.lng}`}><a className="text-b-p text-sm">View on Google Maps</a></Link>
-       <svg  onClick={() => setPopupInfo(null)} className="w-6 h-6 absolute top-0 left-1/2 transform -translate-x-1/2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg>
-    </div>
-    </div>
-     
-       
+          <MobileMapCard popupInfo={popupInfo} setPopupInfo={setPopupInfo} />
         : <></>
         }
           </ReactMapGL>
